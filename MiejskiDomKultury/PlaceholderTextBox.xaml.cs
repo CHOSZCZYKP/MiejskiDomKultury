0d54d7b7b21caacb83993cd9f285c4c80aea7675
Pawel <pawechoszczyk@gmail.com>
2025-05-04 20:13:40 +0200

    Dodaj pliki projektów.

---
+++ MiejskiDomKultury/PlaceholderTextBox.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiejskiDomKultury
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PlaceholderTextBox.xaml
+    /// </summary>
+    public partial class PlaceholderTextBox : UserControl
+    {
+        public PlaceholderTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(PlaceholderTextBox), new PropertyMetadata(string.Empty));
+
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+
+        public static readonly DependencyProperty PlaceholderProperty =
+            DependencyProperty.Register(nameof(Placeholder), typeof(string), typeof(PlaceholderTextBox), new PropertyMetadata(string.Empty));
+    }
+}
