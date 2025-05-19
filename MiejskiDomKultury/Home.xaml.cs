641d37bee0d7ec13adcc045ab90aed75ab7ce90b
KGmen <kappa.chleb@gmail.com>
2025-05-19 19:14:46 +0200

    Dodałem te komunikaty i inne bajery (sporo się dzieje w tej Ostrołęce nie powiem że nie)

--- MiejskiDomKultury/Home.xaml.cs
+++ MiejskiDomKultury/Home.xaml.cs
@@ -26,6 +26,7 @@ namespace MiejskiDomKultury
         public Home()
         {
             InitializeComponent();
+            WyswietlLosoweWiadomosci();
 
         }
 
@@ -47,6 +48,29 @@ namespace MiejskiDomKultury
         private void Film_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new AvailableMovies());
+        }
+
+        public class Wiadomosc
+        {
+            public string Text { get; set; }
+            public string ImagePath { get; set; }
+        }
+
+        private readonly List<Wiadomosc> WszystkieWiadomosci = new List<Wiadomosc>
+        {
+            new Wiadomosc { Text = "Astrolodzy przewidują opad meteorytów dzisiaj o 21:00!", ImagePath = "Assets/news1.webp" },
+            new Wiadomosc { Text = "Kobieta poszukiwana za zdemolowanie sali tanecznej!", ImagePath = "Assets/news2.webp" },
+            new Wiadomosc { Text = "Zapisy na zajęcia z planowania strategii wojennej ruszyły!", ImagePath = "Assets/news3.webp" },
+            new Wiadomosc { Text = "Ostrołęka pogrążona w żałobie. Zmarł Bóbr Bartek", ImagePath = "Assets/news4.webp" },
+            new Wiadomosc { Text = "Parada Imperatora w Ostrołęce - zdjęcia", ImagePath = "Assets/news5.webp" }
+        };
+
+
+
+        private void WyswietlLosoweWiadomosci()
+        {
+            var losowe = WszystkieWiadomosci.OrderBy(x => Guid.NewGuid()).Take(3).ToList();
+            NewsList.ItemsSource = losowe;
         }
     }
 }
