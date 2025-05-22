564c8718daf8bf0bbc5e086afce3955583873f4d
KGmen <kappa.chleb@gmail.com>
2025-05-22 14:45:28 +0200

    👹

--- MiejskiDomKultury/Slider.xaml.cs
+++ MiejskiDomKultury/Slider.xaml.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using static MiejskiDomKultury.Slider;
 
@@ -15,6 +16,9 @@ namespace MiejskiDomKultury
         public ObservableCollection<Die> Dice { get; set; } = new();
         private Random rand = new();
         private Action<int> _onVolumeChange;
+        private int rollEscapes = 0;
+        private bool imageRevealed = false;
+
 
         public Slider()
         {
@@ -60,6 +64,45 @@ namespace MiejskiDomKultury
             VolumeText.Text = "Volume: 0 psuju dobrej zabawy";
             _onVolumeChange?.Invoke(0);
         }
+        private void RollButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Dice.Sum(d => d.Number) <= 50) return;
+
+            double canvasWidth = RollButtonCanvas.ActualWidth;
+            double canvasHeight = RollButtonCanvas.ActualHeight;
+
+            double maxX = canvasWidth - RollButton.ActualWidth;
+            double maxY = canvasHeight - RollButton.ActualHeight;
+
+            if (maxX <= 0 || maxY <= 0) return;
+
+            var rand = new Random();
+            double newLeft = rand.NextDouble() * maxX;
+            double newTop = rand.NextDouble() * maxY;
+
+            Canvas.SetLeft(RollButton, newLeft);
+            Canvas.SetTop(RollButton, newTop);
+            rollEscapes++;
+
+            if (rollEscapes >= 4 && !imageRevealed)
+            {
+                RevealBackgroundImage();
+            }
+        }
+        private void RevealBackgroundImage()
+        {
+            imageRevealed = true;
+
+            var fadeIn = new System.Windows.Media.Animation.DoubleAnimation
+            {
+                From = 0,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(4)
+            };
+
+            BackgroundRevealImage.BeginAnimation(UIElement.OpacityProperty, fadeIn);
+        }
+
 
 
         public class Die : INotifyPropertyChanged
